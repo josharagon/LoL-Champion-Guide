44ba8701f6a8734cc42dba6565015b7ba1c24a91
Josh Aragon <josharagon99@gmail.com>
2021-04-21 18:43:26 -0600

    Add secondary nav bar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -21,6 +21,19 @@ class App extends Component {
           </div>
           <button className='recommend-button'>Recommend me a Champion</button>
         </nav>
+        <nav>
+          <h2>Find Your Champion!</h2>
+          <div className='filter-search'>
+            <select name='filterChampions' id='filter'>
+              <option value='1'>Option1</option>
+              <option value='2'>Option2</option>
+              <option value='3'>Option3</option>
+              <option value='4'>Option4</option>
+              <option value='5'>Option5</option>
+            </select>
+            <input type='text' placeholder='Search for a Champion' className='search'></input>
+          </div>
+        </nav>
       </section>
     )
   }

a6aed01ac5f07823c5c640105ed49b15c323caf2
Josh Aragon <josharagon99@gmail.com>
2021-04-21 18:19:28 -0600

    Complete nav bar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,29 @@
 import './App.css';
 import { Component } from 'react'
+import leagueLogo from '../../img/league-logo.png'
 
-class App extends Component () {
-  constructor () {
+class App extends Component {
+  constructor() {
     super();
     this.state = {
 
     }
   }
 
+
   render() {
-    
+    return (
+      <section>
+        <nav>
+          <div className='logo-container'>
+            <img src={leagueLogo} className='league-logo'></img>
+            <p>eague of Legends <br></br> <br></br>Champion Guide</p>
+          </div>
+          <button className='recommend-button'>Recommend me a Champion</button>
+        </nav>
+      </section>
+    )
   }
 }
+
+export default App;

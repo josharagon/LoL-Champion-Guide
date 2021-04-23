4464ce562f77a511ae3617c63e3d4fe0a735494f
Josh Aragon <josharagon99@gmail.com>
2021-04-22 19:48:48 -0600

    Complete single champion fetch and successfully render objects data on page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -29,43 +29,52 @@ class App extends Component {
   render() {
     return (
       <section>
-        <Link to='/'>
-          <nav>
-            <div className='logo-container'>
-              <img src={leagueLogo} className='league-logo'></img>
-              <p>eague of Legends <br></br> <br></br>Champion Guide</p>
-            </div>
-            <button className='recommend-button'>Recommend me a Champion</button>
-          </nav>
-        </Link>
-        <nav>
-          <h2>Find Your Champion!</h2>
-          <div className='filter-search'>
-            <select name='filterChampions' id='filter'>
-              <option value='1'>Option1</option>
-              <option value='2'>Option2</option>
-              <option value='3'>Option3</option>
-              <option value='4'>Option4</option>
-              <option value='5'>Option5</option>
-            </select>
-            <input type='text' placeholder='Search for a Champion' className='search'></input>
-          </div>
-        </nav>
-        <Container championData={this.state.championData} />
-        <Route
-          exact path ='/:id'
-          render={({ match }) => {
-            const champion = this.state.championData.find(champion => {
-              return champion.id === match.params.id;
-            })
-            if(!champion) {
-              return (<h1>This Champion does not exist!</h1>)
-            }
-            return (
-              <SingleChampionView id={match.params.id} />
-            )
-          }}
-        />
+        <Switch>
+          <Route
+            exact path='/'
+            render={() => {
+              return (
+                <>
+                  <nav>
+                    <div className='logo-container'>
+                      <img src={leagueLogo} className='league-logo'></img>
+                      <p>eague of Legends <br></br> <br></br>Champion Guide</p>
+                    </div>
+                    <button className='recommend-button'>Recommend me a Champion</button>
+                  </nav>
+                  <nav>
+                    <h2>Find Your Champion!</h2>
+                    <div className='filter-search'>
+                      <select name='filterChampions' id='filter'>
+                        <option value='1'>Option1</option>
+                        <option value='2'>Option2</option>
+                        <option value='3'>Option3</option>
+                        <option value='4'>Option4</option>
+                        <option value='5'>Option5</option>
+                      </select>
+                      <input type='text' placeholder='Search for a Champion' className='search'></input>
+                    </div>
+                  </nav>
+                  <Container championData={this.state.championData} />
+                </>
+              )
+            }}
+          />
+          <Route
+            exact path='/:id'
+            render={({ match }) => {
+              const champion = this.state.championData.find(champion => {
+                return champion.id === match.params.id;
+              })
+              if (!champion) {
+                return (<h1>This Champion does not exist!</h1>)
+              }
+              return (
+                <SingleChampionView selectedChampionId={match.params.id} />
+              )
+            }}
+          />
+        </Switch>
       </section>
     )
   }
--- src/components/SingleChampionVIew/SingleChampionView.js
+++ src/components/SingleChampionVIew/SingleChampionView.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './SingleChampionView.css';
+import { fetchSingleChampion } from '../../fetchAPI.js'
 
-const SingleChampionView = () => {
-  return (
-    <article>
+class SingleChampionView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedChampion: {
+        tags: []
+      }
+    }
+  }
 
-    </article>
-  )
+  componentDidMount() {
+    fetchSingleChampion(this.props.selectedChampionId)
+      .then(singleChamp => this.setState({ selectedChampion: singleChamp }))
+  }
+
+  render() {
+    if (!this.state.selectedChampion.tags.length) {
+      return <h1 className='fetching-data'> LOADING DATA... </h1>
+    }
+    return (
+      <article className='singleChamp'>
+        <div className='title-tag'>
+          <h3>{this.state.selectedChampion.title}</h3>
+          <div>
+            <img src={require(`../../img/legacyLogos/${this.state.selectedChampion.tags[0]}_icon.png`).default}></img>
+            <h4>{this.state.selectedChampion.tags[0]}</h4>
+          </div>
+        </div>
+        <h2 className='champion-name'>{this.state.selectedChampion.name}</h2>
+        <p className='champion-blurb'>"{this.state.selectedChampion.blurb}" <span className='read-more'>Read More...</span></p>
+        <p className='champion-lore hidden'>"{this.state.selectedChampion.lore}"</p>
+        <div className='champion-abilities'>
+          <h1 className='abilities-header'>Abilities</h1>
+          <div className='ability-list'>
+            {this.state.selectedChampion.spells.map(ability => {
+              return <h3>{ability.name}</h3>
+            })}
+          </div>
+        </div>
+      </article>
+    )
+  }
 }
 
 

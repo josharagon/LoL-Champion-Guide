58691e0f09e9425fbe388d60d12b0feec1eee1cf
Josh Aragon <josharagon99@gmail.com>
2021-05-09 20:25:15 -0600

    remove broken code

--- cypress/integration/examples/championGuide.spec.js
+++ cypress/integration/examples/championGuide.spec.js
@@ -1,9 +1,3 @@
-import './commands'
-Cypress.on('uncaught:exception', (err, runnable) => {
-// returning false here prevents Cypress from
-// failing the test
-return false
-})
 
 describe('Initial Page', () => {
   beforeEach(() => {
